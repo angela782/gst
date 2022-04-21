f251a5df7d98f4160b4e9b9e04620ded8048c491
angela <aic782@alumnos.unican.es>
2022-04-21 17:16:25 +0200

    ++

--- main.c
+++ main.c
@@ -1,23 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int funcion(int i){
-
-    return i != 5? i:i-1;
-}
-
 int main(){
     
-    int ret=0;
-
-    for (int i=0;i<10;i++){
-        if (funcion(i)==i){
-            printf ("SUCCESS: main funcion %d\n",i);
-        }
-        else{
-            printf ("FAILED: main funcion %d\n",i);
-            ret =-1; 
-        }
-    }
-    return ret;
+    printf("Hello\n");
 }
